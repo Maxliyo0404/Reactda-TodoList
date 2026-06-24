d33b8ca5c709d810d1d76916e04c59d80244558a
Mahliyo <gasandigital13@gmail.com>
2026-06-24 22:01:57 +0500

    TodoList star task

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [task, setTask] = useState([]);
   const addTask = ()=>{
   if (task.trim() === ""){
-    setTask(...task,)
+    setTask(...task, todo)
   }
   }
   return (
