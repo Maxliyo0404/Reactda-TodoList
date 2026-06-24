0d90db1ba4b3740394241d1a6676c66bb84d0f8e
Mahliyo <gasandigital13@gmail.com>
2026-06-24 21:43:29 +0500

    TodoList star onchange

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import React, { useState } from 'react'
 function App() {
   const [todo, setTodo] = useState("");
   const [task, setTask] = useState([]);
+
   return (
    <>
    <div className="cards">
       <div className="card">
         <h1 className="title">TodoList</h1>
         <form  className="form">
-          <input type="text" required placeholder="Add todo......" />
+          <input value={name} onChange={(e)=>setTodo(e.target.value)} type="text" required placeholder="Add todo......" />
           <button type="submit" className="btn"> Submit</button>
         </form>
       </div>
