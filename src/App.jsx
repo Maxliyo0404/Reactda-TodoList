acd35b2948a5c199a5f8cff1adfd8426facb7ec9
Mahliyo <gasandigital13@gmail.com>
2026-06-25 09:06:53 +0500

    TodoList star task todo

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         {task.map((el, index)=>(
          <li key={index} className="item">
            {el}
-           <button className="btn2" type="submit" onClick={()=>deleteBtn}>🗑 Delete</button>
+           <button className="btn2" type="submit" onClick={()=>deleteBtn(index)}>🗑 Delete</button>
         </li>
         ))}
        
