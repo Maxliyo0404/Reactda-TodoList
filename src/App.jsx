d6061db80ade0006455e83e09bb00a8d48cf672f
Mahliyo <gasandigital13@gmail.com>
2026-06-25 10:46:59 +0500

    todoList responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,42 @@ import "./App.css";
 import React, { useState } from 'react'
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [task, setTask] = useState([]);
-  const addTask = ()=>{
-  if (todo.trim() !== ""){
-    setTask([...task, todo])
-    setTodo("")
-  }
-  }
-  const deleteBtn = (index) =>{
-    const newDelete = task.filter((_, i)=> i !== index);
-    setTask(newDelete);
-  }
+//   const [todo, setTodo] = useState("");
+//   const [task, setTask] = useState([]);
+//   const addTask = ()=>{
+//   if (todo.trim() !== ""){
+//     setTask([...task, todo])
+//     setTodo("")
+//   }
+//   }
+//   const deleteBtn = (index) =>{
+//     const newDelete = task.filter((_, i)=> i !== index);
+//     setTask(newDelete);
+//   }
  
-  return (
-   <>
-   <div className="cards">
-      <div className="card">
-        <h1 className="title">TodoList</h1>
-        <form  className="form">
-          <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" required placeholder="Add todo......" />
-          <button type="submit" className="btn" onClick={addTask}>Submit</button>
-        </form>
-      </div>
-      <ul className="list">
-        {task.map((el, index)=>(
-         <li key={index} className="item">
-           {el}
-           <button className="delete-btn" type="submit" onClick={()=>deleteBtn(index)}>🗑 Delete</button>
-        </li>
-        ))}
+//   return (
+//    <>
+//    <div className="cards">
+//       <div className="card">
+//         <h1 className="title">TodoList</h1>
+//         <form  className="form">
+//           <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" required placeholder="Add todo......" />
+//           <button type="submit" className="btn" onClick={addTask}>Submit</button>
+//         </form>
+//       </div>
+//       <ul className="list">
+//         {task.map((el, index)=>(
+//          <li key={index} className="item">
+//            {el}
+//            <button className="delete-btn" type="submit" onClick={()=>deleteBtn(index)}>🗑 Delete</button>
+//         </li>
+//         ))}
        
-      </ul>
-   </div>
+//       </ul>
+//    </div>
 
-   </>
-  )
+//    </>
+//   )
 }
 
 export default App
