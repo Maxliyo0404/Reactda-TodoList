22a541d3678eacfd4e12178eb864bb19f7be8dbf
Mahliyo <gasandigital13@gmail.com>
2026-06-25 10:26:21 +0500

    todoList responsive

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -4,12 +4,17 @@ import React, { useState } from 'react'
 function Todo() {
     const [todo, setTodo] = useState("");
     const [list, setList] = useState([]);
-    
+    const addList = ()=>{
+        if(todo.trim()!== ""){
+         setList([...list, todo]);
+         setTodo("")
+        } 
+    }
   return (
     <div className="box">
         <h1>TodoAdd</h1>
         <form  className="form">
-         <input value={todo}  type="text"required placeholder="Your name....." />
+         <input onChange={(e)=>setTodo(e.target.value)} value={todo}  type="text"required placeholder="Your name....." />
          <button className="btn" type="submit" onClick={()=>}>Send</button>
         </form>
     </div>
